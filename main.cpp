9406e63d0b931598d669338c98944738846f3524
Rorschach2109 <rorschach2109@gmail.com>
2013-08-11 23:39:24 +0200

    Add tests to main.

--- main.cpp
+++ main.cpp
@@ -7,16 +7,25 @@
 #include "StreamInputGraphManagerTest.h"
 #include "StreamOutputGraphManagerTest.h"
 
+#include "NodeModelTest.h"
+#include "EdgeModelTest.h"
+#include "AdjacencyListTest.h"
+#include "AdjacencyListFactoryTest.h"
+
 int main( void )
 {
-	std::cout << "Tests:\n"; 
-	
 	Minotaur::CNodeDtoTest dtoTestNode = Minotaur::CNodeDtoTest();
 	Minotaur::CEdgeDtoTest dtoTestEdge = Minotaur::CEdgeDtoTest();
 	Minotaur::CGraphDtoTest dtoTestGraph = Minotaur::CGraphDtoTest();
 	
 	Minotaur::CStreamInputGraphManagerTest inputGraphManagerTest = Minotaur::CStreamInputGraphManagerTest();
 	Minotaur::CStreamOutputGraphManagerTest outputGraphManagerTest = Minotaur::CStreamOutputGraphManagerTest();
+	
+	Minotaur::CNodeModelTest nodeModelTest = Minotaur::CNodeModelTest();
+	Minotaur::CEdgeModelTest edgeModelTest = Minotaur::CEdgeModelTest();
+	
+	Minotaur::CAdjacencyListTest adjacencyListTest = Minotaur::CAdjacencyListTest();
+	Minotaur::CAdjacencyListFactoryTest adjacencyListFactoryTest = Minotaur::CAdjacencyListFactoryTest();
 	
 	return 0;
 }
