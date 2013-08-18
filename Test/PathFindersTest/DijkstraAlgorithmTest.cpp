8a8f237cdf1d45323ca78e0762a5147526afb9c5
Rorschach2109 <rorschach2109@gmail.com>
2013-08-19 00:03:44 +0200

    #32 Dijkstra Test Implementation
    Stage 1.

---
+++ Test/PathFindersTest/DijkstraAlgorithmTest.cpp
@@ -0,0 +1,32 @@
+/*
+ * This file is part of minotaur.
+ *
+ * File: DijkstraAlgorithmTest.cpp
+ * Brief: Implementation of dijkstra algorithm test class.
+ *
+ * Date: .07.2013
+ * Author: Andrzej Korycinski
+ *
+ * */
+ 
+#include "DijkstraAlgorithmTest.h"
+
+#include "TestDefines.h"
+
+using namespace Minotaur;
+
+CDijkstraAlgorithmTest::CDijkstraAlgorithmTest( void ) : 
+	m_dijkstraAlgorithmTest( CDijkstraAlgorithm() )
+{
+	m_FindShortestPathTest();
+}
+
+CDijkstraAlgorithmTest::~CDijkstraAlgorithmTest( void )
+{
+	
+}
+
+void CDijkstraAlgorithmTest::m_FindShortestPathTest( void )
+{
+	
+}
---
+++ Test/PathFindersTest/DijkstraAlgorithmTest.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of minotaur.
+ *
+ * File: DijkstraAlgorithmTest.h
+ * Brief: Declaration of dijkstra algorithm test class.
+ *
+ * Date: .07.2013
+ * Author: Andrzej Korycinski
+ *
+ * */
+
+#ifndef _MINOTAUR_DIJKSTRAALGORITHMTEST_H_
+#define _MINOTAUR_DIJKSTRAALGORITHMTEST_H_
+
+#include "DijkstraAlgorithm.h"
+
+namespace Minotaur
+{
+	
+class CDijkstraAlgorithmTest
+{
+	private:
+		CDijkstraAlgorithm m_dijkstraAlgorithmTest;
+	
+		void m_FindShortestPathTest( void );
+	
+	public:
+		CDijkstraAlgorithmTest( void );
+		virtual ~CDijkstraAlgorithmTest( void );
+};
+	
+} // namespace Minotaur
+
+#endif /* _MINOTAUR_DIJKSTRAALGORITHMTEST_H_ */
