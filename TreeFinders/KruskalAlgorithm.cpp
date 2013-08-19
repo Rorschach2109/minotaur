20ac79a15447e13178bedb53069643791a6340c8
Rorschach2109 <rorschach2109@gmail.com>
2013-08-19 21:33:51 +0200

    #25 Kruskal Algorithm Implementation.

--- TreeFinders/KruskalAlgorithm.cpp
+++ TreeFinders/KruskalAlgorithm.cpp
@@ -11,7 +11,129 @@
 
 #include "KruskalAlgorithm.h"
 
+#include <limits>
+#include <utility>
+
 using namespace Minotaur;
+
+CKruskalAlgorithm::CEdgeSet::CEdgeSet( const IGraphModel& graphModel ) : 
+	m_graphModel( graphModel )
+{
+	m_graphModelNodesNumber = graphModel.GetNodesNumber();
+	
+	m_mstTreeEdges.clear();
+	m_graphModelRemaningEdges = graphModel.GetGraphModelEdges();
+
+	for ( auto graphModelNode : ( graphModel.GetGraphModelNodes() ) )
+	{
+		unsigned int nodeModelId = graphModelNode.GetNodeId();
+		m_forest[graphModelNode] = nodeModelId;
+	}
+}
+
+CKruskalAlgorithm::CEdgeSet::~CEdgeSet( void )
+{
+	
+}
+
+CEdgeModel CKruskalAlgorithm::CEdgeSet::m_FindCheapestEdge( void )
+{
+	CEdgeModel cheapestEdge = CEdgeModel();
+	double cheapestWeight = std::numeric_limits< double >::infinity();
+	
+	for ( auto edge : m_graphModelRemaningEdges )
+	{
+		double currentEdgeWeight = edge.GetEdgeWeight();
+		if ( currentEdgeWeight < cheapestWeight )
+		{
+			cheapestEdge = edge;
+			cheapestWeight = currentEdgeWeight;
+		}
+	}
+	return cheapestEdge;
+}
+
+void CKruskalAlgorithm::CEdgeSet::m_EraseEdge( CEdgeModel& edgeToErase )
+{
+	for ( auto graphRemainingEdgesIterator = m_graphModelRemaningEdges.begin(); 
+			graphRemainingEdgesIterator != m_graphModelRemaningEdges.end();
+			++graphRemainingEdgesIterator )
+	{
+		if ( edgeToErase == ( *graphRemainingEdgesIterator ) )
+		{
+			m_graphModelRemaningEdges.erase( graphRemainingEdgesIterator );
+			break;
+		}
+	}
+}
+
+void CKruskalAlgorithm::CEdgeSet::m_AddEdgeToForest( const CEdgeModel& edgeToAdd )
+{
+	unsigned int nodeFromId = edgeToAdd.GetNodeFromId();
+	unsigned int nodeToId = edgeToAdd.GetNodeToId();
+	
+	CNodeModel nodeFrom = m_graphModel.GetGraphModelNode(nodeFromId);
+	CNodeModel nodeTo = m_graphModel.GetGraphModelNode(nodeToId);
+	
+	if ( m_forest[nodeFrom] != m_forest[nodeTo] )
+	{
+		unsigned int minNodeId = ( m_forest[nodeFrom] < m_forest[nodeTo] )? m_forest[nodeFrom] : m_forest[nodeTo];
+		unsigned int maxNodeId = ( minNodeId == m_forest[nodeFrom] )? m_forest[nodeTo] : m_forest[nodeFrom];
+		
+		m_forest[nodeFrom] = minNodeId;
+		m_forest[nodeTo] = minNodeId;
+
+		for ( auto& forestNodePair : m_forest )
+		{
+			forestNodePair.second = ( forestNodePair.second == maxNodeId )? minNodeId : forestNodePair.second;
+		}
+		
+		m_mstTreeEdges.push_back(edgeToAdd);
+	}
+	
+}
+
+void CKruskalAlgorithm::CEdgeSet::ExpandMSTTree( void )
+{
+	CEdgeModel cheapestMSTEdge = m_FindCheapestEdge();
+	m_EraseEdge( cheapestMSTEdge );
+	m_AddEdgeToForest( cheapestMSTEdge );
+}
+
+bool CKruskalAlgorithm::CEdgeSet::CanExpandMSTTree( void )
+{
+	bool canExpandMSTTree = false;
+	for ( auto remaningEdge : m_graphModelRemaningEdges )
+	{
+		CNodeModel nodeFrom = m_graphModel.GetGraphModelNode( remaningEdge.GetNodeFromId() );
+		CNodeModel nodeTo = m_graphModel.GetGraphModelNode( remaningEdge.GetNodeToId() );
+		if ( m_forest[nodeFrom] != m_forest[nodeTo] )
+		{
+			canExpandMSTTree = true;
+			break;
+		}
+	}
+	return canExpandMSTTree;
+}
+
+std::shared_ptr < CTreeModel > CKruskalAlgorithm::CEdgeSet::BuildMSTTree( const CNodeModel& nodeRoot )
+{
+	std::vector < std::pair < unsigned int, unsigned int > > mstEdgeDefinition;
+	for ( auto mstEdge : m_mstTreeEdges )
+	{
+		unsigned int nodeFromId = mstEdge.GetNodeFromId();
+		unsigned int nodeToId = mstEdge.GetNodeToId();
+		CNodeModel nodeFrom = m_graphModel.GetGraphModelNode( nodeFromId );
+
+		if ( m_forest[nodeRoot] == m_forest[nodeFrom] )
+		{
+			mstEdgeDefinition.push_back( std::make_pair(nodeFromId, nodeToId) );
+		}
+	}
+	
+	std::shared_ptr < CTreeModel > mstTree( new CTreeModel(m_graphModel, mstEdgeDefinition) );
+	return mstTree;
+}
 
 CKruskalAlgorithm::CKruskalAlgorithm( void ) : 
 	ITreeFinder()
@@ -26,5 +148,11 @@ CKruskalAlgorithm::~CKruskalAlgorithm( void )
 	
 std::shared_ptr < CTreeModel > CKruskalAlgorithm::FindMST( const IGraphModel& graphModel, const CNodeModel& nodeRoot )
 {
-	return nullptr;
+	CEdgeSet edgeSet = CEdgeSet( graphModel );
+	while ( edgeSet.CanExpandMSTTree() )
+	{
+		edgeSet.ExpandMSTTree();
+	}
+	return ( edgeSet.BuildMSTTree( nodeRoot ) );
 }
+
--- TreeFinders/KruskalAlgorithm.h
+++ TreeFinders/KruskalAlgorithm.h
@@ -13,6 +13,12 @@
 #define _MINOTAUR_KRUSKALALGORITHM_H_
 
 #include "ITreeFinder.h"
+#include "IGraphModel.h"
+#include "NodeModel.h"
+#include "EdgeModel.h"
+
+#include <vector>
+#include <map>
 
 namespace Minotaur
 {
@@ -24,6 +30,30 @@ class CKruskalAlgorithm : public ITreeFinder
 		virtual ~CKruskalAlgorithm( void );
 		
 		virtual std::shared_ptr < CTreeModel > FindMST( const IGraphModel& graphModel, const CNodeModel& nodeRoot );
+		
+		class CEdgeSet
+		{
+			private:
+				const IGraphModel& m_graphModel;
+				unsigned int m_graphModelNodesNumber;
+				
+				std::vector < CEdgeModel > m_mstTreeEdges;
+				std::vector < CEdgeModel > m_graphModelRemaningEdges;
+				std::map < CNodeModel, unsigned int, CNodeModel::SNodeModelLess > m_forest;
+				
+				CEdgeModel m_FindCheapestEdge( void );
+				void m_EraseEdge( CEdgeModel& edgeToErase );
+				void m_AddEdgeToForest( const CEdgeModel& edgeToAdd );
+								
+			public:
+				CEdgeSet( void ) = delete;
+				explicit CEdgeSet( const IGraphModel& graphModel );
+				~CEdgeSet( void );
+		
+				void ExpandMSTTree( void );
+				bool CanExpandMSTTree( void );
+				std::shared_ptr < CTreeModel > BuildMSTTree( const CNodeModel& nodeRoot );
+		};
 };
 	
 } // namespace Minotaur
