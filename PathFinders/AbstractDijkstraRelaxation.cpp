8633f72ac37a1a5a408b4104c7a6e059b146494d
Rorschach2109 <rorschach2109@gmail.com>
2013-08-26 21:30:23 +0200

    #61 Abstract Dijkstra Relaxation Provider Implementation.

---
+++ PathFinders/AbstractDijkstraRelaxation.cpp
@@ -0,0 +1,51 @@
+/*
+ * This file is part of minotaur.
+ *
+ * File: DijkstraRelaxation.cpp
+ * Brief: Implementation of abstract dijkstra relaxation class.
+ *
+ * Date: 26.08.2013
+ * Author: Andrzej Korycinski
+ *
+ * */
+
+#include "AbstractDijkstraRelaxation.h"
+#include "PathModel.h"
+
+#include <vector>
+
+using namespace Minotaur;
+
+AbstractDijkstraRelaxation::AbstractDijkstraRelaxation( void )
+{
+	
+}
+
+AbstractDijkstraRelaxation::~AbstractDijkstraRelaxation( void )
+{
+	
+}
+
+std::shared_ptr < CPathModel > AbstractDijkstraRelaxation::BuildPath( const IGraphModel& graphModel, const CNodeModel& nodeFrom, const CNodeModel& nodeTo ) const
+{
+	std::vector < CNodeModel > pathNodes;
+	
+	CNodeModel currentNode = nodeTo;
+	while ( !( currentNode.operator ==(nodeFrom) ) )
+	{
+		if ( ( t_predecesors[currentNode] == currentNode ) && 
+			( !( currentNode.operator ==(nodeFrom) ) ) )
+		{
+			return nullptr;
+		}	
+		
+		pathNodes.push_back(currentNode);
+		currentNode = t_predecesors[currentNode];
+	}
+	
+	pathNodes.push_back(nodeFrom);
+	std::reverse( pathNodes.begin(), pathNodes.end() );
+	
+	std::shared_ptr < CPathModel > shortestPath( new CPathModel( graphModel, pathNodes ) );
+	return shortestPath;
+}
---
+++ PathFinders/AbstractDijkstraRelaxation.h
@@ -0,0 +1,44 @@
+/*
+ * This file is part of minotaur.
+ *
+ * File: DijkstraRelaxation.h
+ * Brief: Declaration of abstract dijkstra relaxation class.
+ *
+ * Date: 26.08.2013
+ * Author: Andrzej Korycinski
+ *
+ * */
+
+#ifndef _MINOTAUR_ABSTRACTDIJKSTRARELAXATION_H_
+#define _MINOTAUR_ABSTRACTDIJKSTRARELAXATION_H_
+
+#include "NodeModel.h"
+#include "IGraphModel.h"
+
+#include <map>
+#include <memory>
+
+namespace Minotaur
+{
+
+class CPathModel;
+	
+class AbstractDijkstraRelaxation
+{
+	protected:
+		mutable std::map < CNodeModel, CNodeModel, CNodeModel::SNodeModelLess > t_predecesors;
+		
+	public:
+		AbstractDijkstraRelaxation( void );
+		virtual ~AbstractDijkstraRelaxation( void );
+		
+		virtual void Reset( const IGraphModel& graphModel, const CNodeModel& nodeFrom ) const = 0;
+		virtual bool Relax( const IGraphModel& graphModel, const CNodeModel& nodeFrom, const CNodeModel& nodeTo ) const = 0;
+		virtual bool IsCheaper( const CNodeModel& lNode, const CNodeModel& rNode ) const = 0;
+		
+		std::shared_ptr < CPathModel > BuildPath( const IGraphModel& graphModel, const CNodeModel& nodeFrom, const CNodeModel& nodeTo ) const;
+};
+	
+} // namespace Minotaur
+
+#endif /* _MINOTAUR_ABSTRACTDIJKSTRARELAXATION_H_ */
