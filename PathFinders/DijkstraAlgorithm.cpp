0eea593080c72e796aac202db3afacb0369c344b
Rorschach2109 <rorschach2109@gmail.com>
2013-08-26 21:31:37 +0200

    #27 Dijkstra Algorithm Implementation.

--- PathFinders/DijkstraAlgorithm.cpp
+++ PathFinders/DijkstraAlgorithm.cpp
@@ -4,17 +4,20 @@
  * File: DijkstraAlgorithm.cpp
  * Brief: Implementation of dijkstra algorithm class.
  *
- * Date: .07.2013
+ * Date: 25.08.2013
  * Author: Andrzej Korycinski
  *
  * */
 
 #include "DijkstraAlgorithm.h"
 
+#include <vector>
+
 using namespace Minotaur;
 
-CDijkstraAlgorithm::CDijkstraAlgorithm( void ) : 
-	IPathFinder()
+CDijkstraAlgorithm::CDijkstraAlgorithm( const AbstractDijkstraRelaxation& relaxationProvider ) : 
+	IPathFinder(),
+	m_relaxationProvider( relaxationProvider )
 {
 	
 }
@@ -23,8 +26,50 @@ CDijkstraAlgorithm::~CDijkstraAlgorithm( void )
 {
 	
 }
-		
+
+CNodeModel CDijkstraAlgorithm::m_GetCheapestNode( const std::set < CNodeModel, CNodeModel::SNodeModelLess >& openNodes )
+{
+	CNodeModel cheapestNode = *(openNodes.begin() );
+	for ( auto openNode : openNodes )
+	{
+		if ( m_relaxationProvider.IsCheaper(openNode, cheapestNode) )
+		{
+			cheapestNode = openNode;
+		}
+	}
+	return cheapestNode;
+}
+
 std::shared_ptr < CPathModel > CDijkstraAlgorithm::FindShortestPath( const IGraphModel& graphModel, const CNodeModel& nodeFrom, const CNodeModel& nodeTo )
 {
-	return nullptr;
+	std::set < CNodeModel, CNodeModel::SNodeModelLess > openNodes;
+	std::set < CNodeModel, CNodeModel::SNodeModelLess > closedNodes;
+	
+	openNodes.insert(nodeFrom);
+	m_relaxationProvider.Reset(graphModel, nodeFrom);
+	
+	while ( !( openNodes.empty() ) )
+	{
+		CNodeModel cheapestNode = m_GetCheapestNode(openNodes);
+		openNodes.erase(cheapestNode);
+		closedNodes.insert(cheapestNode);
+		
+		std::vector < CNodeModel > cheapestNodeNeigbors = graphModel.GetNeighbors(cheapestNode);
+		
+		for ( auto neighborNode : cheapestNodeNeigbors )
+		{
+			if ( ( closedNodes.find(neighborNode) ) != ( closedNodes.end() ) )
+			{
+				continue;
+			}
+
+			bool relaxed = m_relaxationProvider.Relax(graphModel, cheapestNode, neighborNode);
+			if ( relaxed )
+			{
+				openNodes.insert(neighborNode);
+			}
+		}
+	}
+
+	return ( m_relaxationProvider.BuildPath(graphModel, nodeFrom, nodeTo) );
 }
--- PathFinders/DijkstraAlgorithm.h
+++ PathFinders/DijkstraAlgorithm.h
@@ -4,7 +4,7 @@
  * File: DijkstraAlgorithm.h
  * Brief: Declaration of dijkstra algorithm class.
  *
- * Date: .07.2013
+ * Date: 25.08.2013
  * Author: Andrzej Korycinski
  *
  * */
@@ -13,14 +13,23 @@
 #define _MINOTAUR_DIJKSTRAALGORITHM_H_
 
 #include "IPathFinder.h"
+#include "AbstractDijkstraRelaxation.h"
+
+#include <set>
 
 namespace Minotaur
 {
 	
 class CDijkstraAlgorithm : public IPathFinder
 {
+	private:
+		const AbstractDijkstraRelaxation& m_relaxationProvider;
+		
+		CNodeModel m_GetCheapestNode( const std::set < CNodeModel, CNodeModel::SNodeModelLess >& openNodes );
+	
 	public:
-		CDijkstraAlgorithm( void );
+		CDijkstraAlgorithm( void ) = delete;
+		explicit CDijkstraAlgorithm( const AbstractDijkstraRelaxation& relaxationProvider );
 		virtual ~CDijkstraAlgorithm( void );
 		
 		virtual std::shared_ptr < CPathModel > FindShortestPath( const IGraphModel& graphModel, const CNodeModel& nodeFrom, const CNodeModel& nodeTo );
