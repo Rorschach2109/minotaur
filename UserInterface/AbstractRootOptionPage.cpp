72aef635d8d9464ae26c474c67fb6424b2aacc9e
Rorschach2109 <rorschach2109@gmail.com>
2013-11-12 22:06:17 +0100

    #72 Create UI Root Page's Options Pages

--- UserInterface/AbstractRootOptionPage.cpp
+++ UserInterface/AbstractRootOptionPage.cpp
@@ -22,9 +22,13 @@ using namespace Minotaur;
 
 AbstractRootOptionPage::AbstractRootOptionPage( void ) :
 		AbstractUIPage(),
-		t_graphName(""),
-		t_graphStream(nullptr),
-		t_inputGraphManager(nullptr)
+		t_graphName( "" ),
+		t_graphStream( nullptr ),
+		t_algorithmUIPage( nullptr ),
+		t_inputGraphManager( nullptr ),
+		t_outputGraphManager( nullptr ),
+		t_resultGraph( nullptr ),
+		t_resultGraphName( "" )
 {
 
 }
@@ -41,6 +45,17 @@ AbstractRootOptionPage::~AbstractRootOptionPage( void )
 		delete t_inputGraphManager;
 		t_inputGraphManager = nullptr;
 	}
+	if ( nullptr != t_outputGraphManager )
+	{
+		delete t_outputGraphManager;
+		t_outputGraphManager = nullptr;
+	}
+	if ( nullptr != t_algorithmUIPage )
+	{
+		delete t_algorithmUIPage;
+		t_algorithmUIPage = nullptr;
+	}
+	t_resultGraph.reset();
 }
 
 void AbstractRootOptionPage::t_GetGraphName( void ) const
@@ -53,27 +68,22 @@ void AbstractRootOptionPage::t_ProcessChooseAlgorithm( void ) const
 {
 	if ( ( nullptr != t_graphStream ) && ( t_graphStream->good() ) )
 	{
-		CAlgorithmUIPage* algorithmUIPage = new CAlgorithmUIPage(t_inputGraphManager);
-
-		algorithmUIPage->RunPage();
-
-		delete algorithmUIPage;
+		if ( nullptr != t_resultGraph )
+		{
+			t_resultGraph.reset();
+			t_resultGraph = nullptr;
+		}
+		t_algorithmUIPage = new CAlgorithmUIPage(t_inputGraphManager, t_graphName);
+		t_algorithmUIPage->RunPage();
+		t_resultGraphName = t_algorithmUIPage->GetSubGraphName();
+		delete t_algorithmUIPage;
+		t_algorithmUIPage = nullptr;
 	}
 	else
 	{
 		printf("%s", t_noFileLoadedMessage.c_str() );
 	}
-}
-
-void AbstractRootOptionPage::t_ProcessPrintGraph( void ) const
-{
-	if ( ( nullptr != t_graphStream ) && ( t_graphStream->good() ) )
-	{
-	}
-	else
-	{
-		printf("%s", t_noFileLoadedMessage.c_str() );
-	}
+	t_ProcessLoadFile();
 }
 
 void AbstractRootOptionPage::ExecuteOption( void ) const
@@ -81,17 +91,21 @@ void AbstractRootOptionPage::ExecuteOption( void ) const
 	switch( t_optionId )
 	{
 	case ERootOptionId::LOAD_GRAPH_FILE:
+		t_GetGraphName();
 		t_ProcessLoadFile();
 		break;
-	case ERootOptionId::CONVERT_FILE:
-		t_ProcessConvertFile();
+	case ERootOptionId::CONVERT_INPUT_FILE:
+		t_ProcessConvertInputFile();
 		break;
 	case ERootOptionId::CHOOSE_ALGORITHM:
 		t_ProcessChooseAlgorithm();
 		break;
-	case ERootOptionId::PRINT_GRAPH:
-		t_ProcessPrintGraph();
+	case ERootOptionId::PRINT_INPUT_GRAPH:
+		t_ProcessPrintInputGraph();
 		break;
+	case ERootOptionId::PRINT_RESULT_GRAPH:
+		t_ProcessPrintResultGraph();
+		break;	
 	case ERootOptionId::EXIT:
 		break;
 	default:
--- UserInterface/AbstractRootOptionPage.h
+++ UserInterface/AbstractRootOptionPage.h
@@ -16,19 +16,24 @@
 
 #include <string>
 #include <istream>
+#include <memory>
 
 namespace Minotaur
 {
 
 class AbstractInputGraphManager;
 class AbstractOutputGraphManager;
+class CAlgorithmUIPage;
+class IGraphModel;
+class CSubGraphToGraphAdapter;
 
 enum ERootOptionId
 {
 	LOAD_GRAPH_FILE = 1,
-	CONVERT_FILE,
+	CONVERT_INPUT_FILE,
 	CHOOSE_ALGORITHM,
-	PRINT_GRAPH,
+	PRINT_INPUT_GRAPH,
+	PRINT_RESULT_GRAPH,
 	EXIT = EXIT_PAGE
 };
 
@@ -37,15 +42,19 @@ class AbstractRootOptionPage : public AbstractUIPage
 	protected:
 		mutable std::string t_graphName;
 		mutable std::istream* t_graphStream;
-
+		mutable CAlgorithmUIPage* t_algorithmUIPage;
 		mutable AbstractInputGraphManager* t_inputGraphManager;
-
+		mutable AbstractOutputGraphManager* t_outputGraphManager;
+		mutable std::shared_ptr < CSubGraphToGraphAdapter > t_resultGraph;
+		mutable std::string t_resultGraphName;
+		
 		void t_GetGraphName( void ) const;
 		void t_ProcessChooseAlgorithm( void ) const;
-		void t_ProcessPrintGraph( void ) const;
 
 		virtual void t_ProcessLoadFile( void ) const = 0;
-		virtual void t_ProcessConvertFile( void ) const = 0;
+		virtual void t_ProcessConvertInputFile( void ) const = 0;
+		virtual void t_ProcessPrintInputGraph( void ) const = 0;
+		virtual void t_ProcessPrintResultGraph( void ) const = 0;
 
 	public:
 		AbstractRootOptionPage( void );
